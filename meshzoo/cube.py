dbc054ba6068459a0ea6ef13cd2bf0605018ff5f
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-23 19:20:51 +0200

    better interface for rectangle, cube

--- meshzoo/cube.py
+++ meshzoo/cube.py
@@ -5,19 +5,21 @@
 import numpy as np
 
 
-def create_mesh(ax=1.0, ay=1.0, az=1.0, nx=11, ny=11, nz=11):
+def create_mesh(
+        xmin=0.0, xmax=1.0,
+        ymin=0.0, ymax=1.0,
+        zmin=0.0, zmax=1.0,
+        nx=11, ny=11, nz=11
+        ):
     '''Canonical tetrahedrization of the cube.
     Input:
     Edge lenghts of the cube
     Number of nodes along the edges.
     '''
-    N = [nx, ny, nz]
-    l = [ax, ay, az]
-
     # Generate suitable ranges for parametrization
-    x_range = np.linspace(0.0, l[0], N[0])
-    y_range = np.linspace(0.0, l[1], N[1])
-    z_range = np.linspace(0.0, l[2], N[2])
+    x_range = np.linspace(xmin, xmax, nx)
+    y_range = np.linspace(ymin, ymax, ny)
+    z_range = np.linspace(zmin, zmax, nz)
 
     # Create the vertices.
     num_nodes = len(x_range) * len(y_range) * len(z_range)
@@ -34,69 +36,69 @@ def create_mesh(ax=1.0, ay=1.0, az=1.0, nx=11, ny=11, nz=11):
     # and 12 ways to split it into 6 tetrahedra.
     # See <http://private.mcnet.ch/baumann/Splitting%20a%20cube%20in%20tetrahedras2.htm>.
     # Also interesting: <http://en.wikipedia.org/wiki/Marching_tetrahedrons>.
-    num_cells = 5 * (N[0]-1) * (N[1]-1) * (N[2]-1)
+    num_cells = 5 * (nx-1) * (ny-1) * (nz-1)
     cellNodes = np.empty(num_cells, dtype=np.dtype((int, 4)))
     l = 0
-    for i in range(N[0] - 1):
-        for j in range(N[1] - 1):
-            for k in range(N[2] - 1):
+    for i in range(nx - 1):
+        for j in range(ny - 1):
+            for k in range(nz - 1):
                 # Switch the element styles to make sure the edges match at
                 # the faces of the cubes.
                 if (i + j + k) % 2 == 0:
                     cellNodes[l] = np.array([
-                        N[2] * (N[1]*i     + j  ) + k,
-                        N[2] * (N[1]*i     + j+1) + k,
-                        N[2] * (N[1]*(i+1) + j  ) + k,
-                        N[2] * (N[1]*i     + j  ) + k+1
+                        nz * (ny*i     + j  ) + k,
+                        nz * (ny*i     + j+1) + k,
+                        nz * (ny*(i+1) + j  ) + k,
+                        nz * (ny*i     + j  ) + k+1
                         ])
                     l += 1
-                    cellNodes[l] = np.array([N[2] * (N[1]*i     + j+1) + k,
-                                             N[2] * (N[1]*(i+1) + j+1) + k,
-                                             N[2] * (N[1]*(i+1) + j  ) + k,
-                                             N[2] * (N[1]*(i+1) + j+1) + k+1])
+                    cellNodes[l] = np.array([nz * (ny*i     + j+1) + k,
+                                             nz * (ny*(i+1) + j+1) + k,
+                                             nz * (ny*(i+1) + j  ) + k,
+                                             nz * (ny*(i+1) + j+1) + k+1])
                     l += 1
-                    cellNodes[l] = np.array([N[2] * (N[1]*i     + j+1) + k,
-                                             N[2] * (N[1]*(i+1) + j  ) + k,
-                                             N[2] * (N[1]*i     + j  ) + k+1,
-                                             N[2] * (N[1]*(i+1) + j+1) + k+1])
+                    cellNodes[l] = np.array([nz * (ny*i     + j+1) + k,
+                                             nz * (ny*(i+1) + j  ) + k,
+                                             nz * (ny*i     + j  ) + k+1,
+                                             nz * (ny*(i+1) + j+1) + k+1])
                     l += 1
-                    cellNodes[l] = np.array([N[2] * (N[1]*i     + j+1) + k,
-                                             N[2] * (N[1]*i     + j  ) + k+1,
-                                             N[2] * (N[1]*i     + j+1) + k+1,
-                                             N[2] * (N[1]*(i+1) + j+1) + k+1])
+                    cellNodes[l] = np.array([nz * (ny*i     + j+1) + k,
+                                             nz * (ny*i     + j  ) + k+1,
+                                             nz * (ny*i     + j+1) + k+1,
+                                             nz * (ny*(i+1) + j+1) + k+1])
                     l += 1
-                    cellNodes[l] = np.array([N[2] * (N[1]*(i+1) + j  ) + k,
-                                             N[2] * (N[1]*i     + j  ) + k+1,
-                                             N[2] * (N[1]*(i+1) + j+1) + k+1,
-                                             N[2] * (N[1]*(i+1) + j  ) + k+1])
+                    cellNodes[l] = np.array([nz * (ny*(i+1) + j  ) + k,
+                                             nz * (ny*i     + j  ) + k+1,
+                                             nz * (ny*(i+1) + j+1) + k+1,
+                                             nz * (ny*(i+1) + j  ) + k+1])
                     l += 1
                 else:
                     # Like the previous one, but flipped along the first
                     # coordinate: i+1 -> i, i -> i+1.
-                    cellNodes[l] = np.array([N[2] * ( N[1]*(i+1) + j   ) + k,
-                                             N[2] * ( N[1]*(i+1) + j+1 ) + k,
-                                             N[2] * ( N[1]*i     + j   ) + k,
-                                             N[2] * ( N[1]*(i+1) + j   ) + k+1])
+                    cellNodes[l] = np.array([nz * ( ny*(i+1) + j   ) + k,
+                                             nz * ( ny*(i+1) + j+1 ) + k,
+                                             nz * ( ny*i     + j   ) + k,
+                                             nz * ( ny*(i+1) + j   ) + k+1])
                     l += 1
-                    cellNodes[l] = np.array([N[2] * ( N[1]*(i+1) + j+1 ) + k,
-                                             N[2] * ( N[1]*i     + j+1 ) + k,
-                                             N[2] * ( N[1]*i     + j   ) + k,
-                                             N[2] * ( N[1]*i     + j+1 ) + k+1])
+                    cellNodes[l] = np.array([nz * ( ny*(i+1) + j+1 ) + k,
+                                             nz * ( ny*i     + j+1 ) + k,
+                                             nz * ( ny*i     + j   ) + k,
+                                             nz * ( ny*i     + j+1 ) + k+1])
                     l += 1
-                    cellNodes[l] = np.array([N[2] * ( N[1]*(i+1) + j+1 ) + k,
-                                             N[2] * ( N[1]*i     + j   ) + k,
-                                             N[2] * ( N[1]*(i+1) + j   ) + k+1,
-                                             N[2] * ( N[1]*i     + j+1 ) + k+1])
+                    cellNodes[l] = np.array([nz * ( ny*(i+1) + j+1 ) + k,
+                                             nz * ( ny*i     + j   ) + k,
+                                             nz * ( ny*(i+1) + j   ) + k+1,
+                                             nz * ( ny*i     + j+1 ) + k+1])
                     l += 1
-                    cellNodes[l] = np.array([N[2] * ( N[1]*(i+1) + j+1 ) + k,
-                                             N[2] * ( N[1]*(i+1) + j   ) + k+1,
-                                             N[2] * ( N[1]*(i+1) + j+1 ) + k+1,
-                                             N[2] * ( N[1]*i     + j+1 ) + k+1])
+                    cellNodes[l] = np.array([nz * ( ny*(i+1) + j+1 ) + k,
+                                             nz * ( ny*(i+1) + j   ) + k+1,
+                                             nz * ( ny*(i+1) + j+1 ) + k+1,
+                                             nz * ( ny*i     + j+1 ) + k+1])
                     l += 1
-                    cellNodes[l] = np.array([N[2] * ( N[1]*i     + j   ) + k,
-                                             N[2] * ( N[1]*(i+1) + j   ) + k+1,
-                                             N[2] * ( N[1]*i     + j+1 ) + k+1,
-                                             N[2] * ( N[1]*i     + j   ) + k+1])
+                    cellNodes[l] = np.array([nz * ( ny*i     + j   ) + k,
+                                             nz * ( ny*(i+1) + j   ) + k+1,
+                                             nz * ( ny*i     + j+1 ) + k+1,
+                                             nz * ( ny*i     + j   ) + k+1])
                     l += 1
 
     return nodes, cellNodes
