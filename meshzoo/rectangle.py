a8fa49cf8d606ef08728611b39dc2135cb04027a
Nico Schlömer <nico.schloemer@gmail.com>
2016-08-10 22:33:09 +0200

    rectangle: use zigzag by default

--- meshzoo/rectangle.py
+++ meshzoo/rectangle.py
@@ -10,7 +10,7 @@ def create_mesh(
         xmin=0.0, xmax=1.0,
         ymin=0.0, ymax=1.0,
         nx=11, ny=11,
-        zigzag=False
+        zigzag=True
         ):
     if zigzag:
         return _zigzag(xmin, xmax, ymin, ymax, nx, ny)
