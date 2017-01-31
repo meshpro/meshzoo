a8812f148d53b0ed7b31bbedefb30d76c967838a
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-31 15:07:57 +0100

    another improvement for the tube

--- meshzoo/helpers.py
+++ meshzoo/helpers.py
@@ -1076,12 +1076,13 @@ def tube(length=5.0, radius=1.0, n=30):
     v_range = numpy.linspace(-0.5*length, 0.5*length, num=nw)
 
     # Create the vertices.
-    X = radius * numpy.cos(u_range)
-    Y = radius * numpy.sin(u_range)
-    nodes = numpy.array([
-        [x, y, v]
-        for x, y in zip(X, Y)
-        for v in v_range
+    proto_nodes = numpy.dstack(
+        numpy.meshgrid(u_range, v_range, indexing='ij')
+        ).reshape(-1, 2)
+    nodes = numpy.column_stack([
+        radius * numpy.cos(proto_nodes[:, 0]),
+        radius * numpy.sin(proto_nodes[:, 0]),
+        proto_nodes[:, 1]
         ])
 
     # create the elements (cells)
@@ -1090,9 +1091,10 @@ def tube(length=5.0, radius=1.0, n=30):
         for j in range(nw - 1):
             elems.append([i*nw + j, (i + 1)*nw + j + 1, i * nw + j + 1])
             elems.append([i*nw + j, (i + 1)*nw + j,     (i + 1)*nw + j + 1])
+
     # close the geometry
     for j in range(nw - 1):
         elems.append([(n - 1)*nw + j, j + 1, (n - 1)*nw + j + 1])
         elems.append([(n - 1)*nw + j, j, j + 1])
 
-    return numpy.array(nodes), numpy.array(elems)
+    return nodes, numpy.array(elems)
