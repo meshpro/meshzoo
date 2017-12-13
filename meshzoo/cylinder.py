7abc5ed08ca9461b54e6c107768a32bebd55d55f
Nico Schlömer <nico.schloemer@gmail.com>
2017-12-13 13:27:18 +0100

    simplify cylinder

--- meshzoo/cylinder.py
+++ meshzoo/cylinder.py
@@ -22,30 +22,29 @@ def cylinder():
         - 0.5 * width
 
     # Create the vertices. This is based on the parameterization
-    # of the M\'obius strip as given in
+    # of the Möbius strip as given in
     # <http://en.wikipedia.org/wiki/M%C3%B6bius_strip#Geometry_and_topology>
-    num_nodes = nl * nw
-    nodes = numpy.empty(num_nodes, dtype=numpy.dtype((float, 3)))
-    k = 0
-    for u in u_range:
-        for v in v_range:
-            nodes[k] = numpy.array([numpy.cos(u), numpy.sin(u), v])
-            k += 1
+    nodes = numpy.array([
+            [numpy.cos(u), numpy.sin(u), v]
+            for u in u_range
+            for v in v_range
+            ])
 
     # create the elements (cells)
-    numelems = 2 * nl * (nw-1)
-    elems = numpy.zeros([numelems, 3], dtype=int)
-    k = 0
-    for i in range(nl - 1):
-        for j in range(nw - 1):
-            elems[k] = [i*nw + j, (i + 1)*nw + j + 1, i * nw + j + 1]
-            elems[k+1] = [i*nw + j, (i + 1)*nw + j, (i + 1)*nw + j + 1]
-            k += 2
+    elems = numpy.concatenate([
+        [
+            [i*nw + j, (i + 1)*nw + j + 1, i * nw + j + 1],
+            [i*nw + j, (i + 1)*nw + j, (i + 1)*nw + j + 1],
+        ]
+        for i in range(nl - 1)
+        for j in range(nw - 1)
+        ] + [
+        # close the geometry
+        [
+            [(nl - 1)*nw + j, j + 1, (nl - 1)*nw + j + 1],
+            [(nl - 1)*nw + j, j, j + 1],
+        ]
+        for j in range(nw - 1)
+        ])
 
-    # close the geometry
-    for j in range(nw - 1):
-        elems[k] = [(nl - 1)*nw + j, j + 1, (nl - 1)*nw + j + 1]
-        elems[k+1] = [(nl - 1)*nw + j, j, j + 1]
-        k += 2
-
-    return numpy.array(nodes), numpy.array(elems)
+    return nodes, elems
