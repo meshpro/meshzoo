83d20c53e29ae17b920b6d19695c8d1b48c560e9
Nico Schlömer <nico.schloemer@gmail.com>
2018-09-26 17:38:19 +0200

    some lint fixes

--- meshzoo/__init__.py
+++ meshzoo/__init__.py
@@ -2,15 +2,32 @@
 #
 from meshzoo.__about__ import __version__, __author__, __author_email__, __website__
 
-# pylint: disable=wildcard-import
-from .cube import *
-from .hexagon import *
-from .moebius import *
-from .rectangle import *
-from .simple_arrow import *
-from .simple_shell import *
-from .sphere import *
-from .triangle import *
-from .tube import *
+from .cube import cube
+from .hexagon import hexagon
+from .moebius import moebius
+from .rectangle import rectangle
+from .simple_arrow import simple_arrow
+from .simple_shell import simple_shell
+from .sphere import uv_sphere, iso_sphere
+from .triangle import triangle
+from .tube import tube
 
 from .helpers import *
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__website__",
+    #
+    "cube",
+    "hexagon",
+    "moebius",
+    "rectangle",
+    "simple_arrow",
+    "simple_shell",
+    "uv_sphere",
+    "iso_sphere",
+    "triangle",
+    "tube",
+]
