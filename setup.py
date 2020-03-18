58a5aabe00cf57f0243ae0f1d365c62669610e5e
Nico Schlömer <nico.schloemer@gmail.com>
2020-03-18 09:50:35 +0100

    modernize

--- setup.py
+++ setup.py
@@ -1,38 +1,4 @@
-import os
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-# https://packaging.python.org/single_source_version/
-base_dir = os.path.abspath(os.path.dirname(__file__))
-about = {}
-with open(os.path.join(base_dir, "meshzoo", "__about__.py"), "rb") as f:
-    exec(f.read(), about)
-
-setup(
-    name="meshzoo",
-    version=about["__version__"],
-    author=about["__author__"],
-    author_email=about["__author_email__"],
-    packages=find_packages(),
-    description="A collection of meshes for numerical computation",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/nschloe/meshzoo",
-    download_url="https://github.com/nschloe/meshzoo/releases",
-    license=about["__license__"],
-    platforms="any",
-    install_requires=["numpy"],
-    extras_require={"all": ["matplotlib"], "plot": ["matplotlib"]},
-    classifiers=[
-        about["__status__"],
-        about["__license__"],
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
-)
+if __name__ == "__main__":
+    setup()
