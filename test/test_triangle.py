dbef9e57e6c6ad0d1988a648137e958ef2cf2e0e
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-25 13:25:33 +0200

    bit faster triangle

--- test/test_triangle.py
+++ test/test_triangle.py
@@ -1,4 +1,5 @@
 import numpy
+
 import meshzoo
 from helpers import _near_equal
 
@@ -8,3 +9,9 @@ def test_triangle():
     assert len(points) == 15
     assert _near_equal(numpy.sum(points, axis=0), [0.0, 0.0])
     assert len(cells) == 16
+
+
+if __name__ == "__main__":
+    points, cells = meshzoo.triangle(5000)
+    # import meshio
+    # meshio.write_points_cells('triangle.vtk', points, {'triangle': cells})
