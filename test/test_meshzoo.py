d9a0166624de352ae454a16d7c4e62f7dfe862f4
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-25 12:25:57 +0200

    move triangle test into its own file

--- test/test_meshzoo.py
+++ test/test_meshzoo.py
@@ -138,13 +138,6 @@ def test_simple_shell():
     assert len(cells) == 4
 
 
-def test_triangle():
-    points, cells = meshzoo.triangle(4)
-    assert len(points) == 15
-    assert _near_equal(numpy.sum(points, axis=0), [0.0, 0.0, 0.0])
-    assert len(cells) == 16
-
-
 def test_tube():
     points, cells = meshzoo.tube(n=10)
     assert len(points) == 20
