d9c465f4ea4efaf7173156463a80912729126126
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-25 12:34:51 +0200

    fix typo

--- meshzoo/__about__.py
+++ meshzoo/__about__.py
@@ -5,6 +5,6 @@ except ImportError:
     import importlib_metadata as metadata
 
 try:
-    __version__ = metadata.version("optimesh")
+    __version__ = metadata.version("meshzoo")
 except Exception:
     __version__ = "unknown"
