dcb230a19521d9a413ae06b127c44f4bfffc6d30
Nico Schlömer <nico.schloemer@gmail.com>
2020-06-03 11:44:19 +0200

    test fix

--- test/test_disk.py
+++ test/test_disk.py
@@ -12,7 +12,7 @@ def test_ngon_disk():
     points, cells = meshzoo.ngon_disk(9, 8)
     assert len(points) == 325
     assert len(cells) == 576
-    meshzoo.save2d("9gon_disk.svg", points, cells)
+    # meshzoo.save2d("9gon_disk.svg", points, cells)
 
 
 if __name__ == "__main__":
