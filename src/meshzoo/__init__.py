c9180c7a98dab4f87a5b739f47dcfa9cd599e8bb
Andreas D <a.dutzler@gmail.com>
2021-07-23 11:26:10 +0200

    Update __init__.py
    run black after isort

--- src/meshzoo/__init__.py
+++ src/meshzoo/__init__.py
@@ -2,8 +2,7 @@ from .__about__ import __version__
 from ._ball import ball_hexa, ball_tetra
 from ._cube import cube, cube_hexa, cube_tetra
 from ._disk import disk, disk_quad
-from ._helpers import (create_edges, insert_midpoints_edges, plot2d, save2d,
-                       show2d)
+from ._helpers import create_edges, insert_midpoints_edges, plot2d, save2d, show2d
 from ._moebius import moebius
 from ._ngon import ngon
 from ._rectangle import rectangle, rectangle_quad, rectangle_tri
