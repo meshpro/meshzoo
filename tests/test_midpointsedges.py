16f3533957b4bbadcc0b3c7a7096214a6cc279f7
Andreas D <a.dutzler@gmail.com>
2021-07-23 11:11:09 +0200

    enhance tests for insert_midpoints_edges
    assert also length of points before insertion of midpoints; assert full shape of cells before and after insertion of midpoints

--- tests/test_midpointsedges.py
+++ tests/test_midpointsedges.py
@@ -5,39 +5,59 @@ import meshzoo
 
 def test_midpoints_edges_tri():
     points, cells = meshzoo.rectangle_tri(a0=(0, 0), a1=(1, 1), n=2)
+
+    assert len(points) == 4
+    assert cells.shape == (2, 3)
+
     points_new, cells_new = meshzoo.insert_midpoints_edges(
         points, cells, cell_type="triangle"
     )
+
     assert len(points_new) == 9
-    assert cells_new.shape[1] == 6
+    assert cells_new.shape == (6, 6)
 
 
 def test_midpoints_edges_tetra():
     points, cells = meshzoo.cube_tetra(a0=(0, 0, 0), a1=(1, 1, 1), n=2)
+
+    assert len(points) == 8
+    assert cells.shape == (5, 4)
+
     points_new, cells_new = meshzoo.insert_midpoints_edges(
         points, cells, cell_type="tetra"
     )
+
     assert len(points_new) == 26
-    assert cells_new.shape[1] == 10
+    assert cells_new.shape == (5, 10)
 
 
 def test_midpoints_edges_quad():
     points, cells = meshzoo.rectangle_quad(a0=(0, 0), a1=(1, 1), n=3)
+
+    assert len(points) == 9
+    assert cells.shape == (4, 4)
+
     points_new, cells_new = meshzoo.insert_midpoints_edges(
         points, cells, cell_type="quad"
     )
+
     assert len(points_new) == 21
-    assert cells_new.shape[1] == 8
+    assert cells_new.shape == (8, 8)
 
 
 def test_midpoints_edges_hexa():
     points, cells = meshzoo.cube_hexa(a0=(0, 0, 0), a1=(1, 1, 1), n=3)
+
+    assert len(points) == 27
+    assert cells.shape == (8, 8)
+
     points_new, cells_new = meshzoo.insert_midpoints_edges(
         points, cells, cell_type="hexahedron"
     )
+
     assert len(points_new) == 81
-    assert cells_new.shape[1] == 20
+    assert cells_new.shape == (8, 20)
 
 
 if __name__ == "__main__":
-    test_midpoints_edges_hexa()
+    test_midpoints_edges_tetra()
